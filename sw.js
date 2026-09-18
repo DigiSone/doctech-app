11c370234f1eecf85f1072541aaef32ccf802859
DIGISONE <digisoneglobal@gmail.com>
2026-09-18 02:47:28 +0000

    release(cache-purge): force cloudflare cache bust, kill stale sw, visual live badge 18 Sep 2026 • 02:47 AM

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch', function(e){});
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => {
+  caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))));
+  self.registration.unregister().then(() => self.clients.claim());
+});
