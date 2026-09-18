b5d8bbf07cde5b672b9df109597d6ad65626a646
DIGISONE <digisoneglobal@gmail.com>
2026-09-18 03:05:29 +0000

    fix(build-error): restore 35 services in dataset, multi-page routing, and clean build [18 Sep 2026 • 03:05 AM]

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,2 @@
 self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => {
-  caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))));
-  self.registration.unregister().then(() => self.clients.claim());
-});
+self.addEventListener('activate', (e) => { caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))); self.registration.unregister().then(() => self.clients.claim()); });
